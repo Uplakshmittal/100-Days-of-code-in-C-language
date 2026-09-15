0f918336a45f2419f8a2db631679840a8eeab4ea
Uplakshmittal <Uplaksh.35578@stu.upes.ac.in>
2026-09-15 20:59:20 +0530

    Add files via upload

---
+++ Q6.c
@@ -0,0 +1,18 @@
+//Q6: Write a program to swap two numbers using a third variable.
+
+
+
+#include<stdio.h>
+
+int main()
+{
+    int a, b, c;
+    printf("enter the numbers: ");
+    scanf("%d %d", &a, &b);
+    c = a;
+    a = b;
+    b = c;
+
+    printf("%d %d", a, b);
+    return 0;
+}
